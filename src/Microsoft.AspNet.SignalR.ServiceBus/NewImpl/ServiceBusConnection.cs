8669bf54c9bd30ccf08d2dc3c4911c30e33dd515
David Fowler <davidfowl@gmail.com>
2013-02-07 23:36:49 -0800

    Initial implementation of the service bus rewrite.

---
+++ src/Microsoft.AspNet.SignalR.ServiceBus/NewImpl/ServiceBusConnection.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.ServiceBus.Infrastructure;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Microsoft.AspNet.SignalR.ServiceBus
+{
+    public class ServiceBusConnection : IDisposable
+    {
+        private const int ReceiveBatchSize = 1000;
+
+        private readonly NamespaceManager _namespaceManager;
+        private readonly MessagingFactory _factory;
+        private readonly string _connectionString;
+
+        public ServiceBusConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+            _namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+            _factory = MessagingFactory.CreateFromConnectionString(connectionString);
+        }
+
+        public IDisposable Subscribe(IList<string> topicNames, Action<string, IEnumerable<BrokeredMessage>> handler)
+        {
+            var subscription = new Subscription(_namespaceManager);
+
+            foreach (var topicPath in topicNames)
+            {
+                if (!_namespaceManager.TopicExists(topicPath))
+                {
+                    _namespaceManager.CreateTopic(topicPath);
+                }
+
+                // Create a random subscription
+                string subName = Guid.NewGuid().ToString();
+                SubscriptionDescription topicSubscription = _namespaceManager.CreateSubscription(topicPath, subName);
+                string subscriptionEntityPath = SubscriptionClient.FormatSubscriptionPath(topicPath, subName);
+
+                MessageReceiver receiver = _factory.CreateMessageReceiver(subscriptionEntityPath);
+
+                subscription.TopicPath = topicPath;
+                subscription.Name = subName;
+                subscription.Receivers.Add(receiver);
+
+                ReceiveMessages(topicPath, receiver, handler);
+            }
+
+            return subscription;
+        }
+
+        public Task Publish(string topicName, Stream stream)
+        {
+            var client = TopicClient.CreateFromConnectionString(_connectionString, topicName);
+            var message = new BrokeredMessage(stream, ownsStream: true)
+            {
+                TimeToLive = TimeSpan.FromMinutes(1)
+            };
+
+            return client.SendAsync(message);
+        }
+
+        public void Dispose()
+        {
+            // REVIEW: What do we dipose here
+        }
+
+        private void ReceiveMessages(string topicPath, MessageReceiver receiver, Action<string, IEnumerable<BrokeredMessage>> handler)
+        {
+        receive:
+
+            IAsyncResult result = null;
+
+            try
+            {
+                result = receiver.BeginReceiveBatch(ReceiveBatchSize, ar =>
+                {
+                    bool backOff = false;
+
+                    try
+                    {
+                        if (ar.CompletedSynchronously)
+                        {
+                            return;
+                        }
+
+                        handler(topicPath, receiver.EndReceiveBatch(ar));
+                    }
+                    catch (ServerBusyException)
+                    {
+                        // Too busy so back off   
+                        backOff = true;
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        // Closed
+                        return;
+                    }
+
+                    if (backOff)
+                    {
+                        TaskAsyncHelper.Delay(TimeSpan.FromSeconds(20))
+                                       .Then(() => ReceiveMessages(topicPath, receiver, handler));
+                    }
+                    else
+                    {
+                        ReceiveMessages(topicPath, receiver, handler);
+                    }
+                },
+                null);
+            }
+            catch (OperationCanceledException)
+            {
+                // Closed
+                return;
+            }
+
+            if (result.CompletedSynchronously)
+            {
+                handler(topicPath, receiver.EndReceiveBatch(result));
+                goto receive;
+            }
+        }
+
+        private class Subscription : IDisposable
+        {
+            private readonly NamespaceManager _namespaceManager;
+
+            public Subscription(NamespaceManager namespaceManager)
+            {
+                _namespaceManager = namespaceManager;
+                Receivers = new List<MessageReceiver>();
+            }
+
+            public string TopicPath { get; set; }
+            public string Name { get; set; }
+            public List<MessageReceiver> Receivers { get; private set; }
+
+            public void Dispose()
+            {
+                foreach (var receiver in Receivers)
+                {
+                    receiver.Close();
+                }
+
+                _namespaceManager.DeleteSubscription(TopicPath, Name);
+            }
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.SignalR.ServiceBus/TopicMessageBus2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Messaging;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Microsoft.AspNet.SignalR.ServiceBus
+{
+    public class TopicMessageBus2 : ScaleoutMessageBus
+    {
+        private IDisposable _subscription;
+        private ServiceBusConnection _connection;
+        private readonly string[] _topics;
+
+        public TopicMessageBus2(string connectionString, string topicPrefix, int numberOfTopics, IDependencyResolver resolver)
+            : base(resolver)
+        {
+            _connection = new ServiceBusConnection(connectionString);
+
+            _topics = Enumerable.Range(0, numberOfTopics)
+                                .Select(topicIndex => topicPrefix + "_" + topicIndex)
+                                .ToArray();
+
+            _subscription = _connection.Subscribe(_topics, OnMessage);
+        }
+
+
+        protected override Task Send(IList<Message> messages)
+        {
+            var taskCompletionSource = new TaskCompletionSource<object>();
+
+            // Group messages by source (connection id)
+            var messagesBySource = messages.GroupBy(m => m.Source);
+
+            SendImpl(messagesBySource.GetEnumerator(), taskCompletionSource);
+
+            return taskCompletionSource.Task;
+        }
+
+        private void SendImpl(IEnumerator<IGrouping<string, Message>> enumerator, TaskCompletionSource<object> taskCompletionSource)
+        {
+            if (!enumerator.MoveNext())
+            {
+                taskCompletionSource.TrySetResult(null);
+            }
+            else
+            {
+                IGrouping<string, Message> group = enumerator.Current;
+
+                // Get the channel index we're going to use for this message
+                int index = Md5Hash.Compute32bitHashCode(group.Key) % _topics.Length;
+
+                string topic = _topics[index];
+
+                Stream stream = FastMessageSerializer.GetStream(group);
+
+                // Increment the channel number
+                _connection.Publish(topic, stream)
+                                   .Then((enumer, tcs) => SendImpl(enumer, tcs), enumerator, taskCompletionSource)
+                                   .ContinueWithNotComplete(taskCompletionSource);
+            }
+        }
+
+        private void OnMessage(string topicName, IEnumerable<BrokeredMessage> messages)
+        {
+            foreach (var m in messages)
+            {
+                var internalMessages = FastMessageSerializer.GetMessages(m.GetBody<Stream>())
+                                                            .ToArray();
+
+                OnReceived(topicName, (ulong)m.SequenceNumber, internalMessages);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_subscription != null)
+                {
+                    _subscription.Dispose();
+                }
+
+                if (_connection != null)
+                {
+                    _connection.Dispose();
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
